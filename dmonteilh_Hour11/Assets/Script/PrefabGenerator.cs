e78590e7c1b987a33b29120a4b0806e31aba6615
Ka1and <darrianmonteilh@gmail.com>
2024-11-17 23:56:37 -0800

    Update PrefabGenerator.cs

--- dmonteilh_Hour11/Assets/Script/PrefabGenerator.cs
+++ dmonteilh_Hour11/Assets/Script/PrefabGenerator.cs
@@ -5,6 +5,18 @@ using UnityEngine;
 public class PrefabGenerator : MonoBehaviour
 {
     public GameObject prefab;
+    public int numberOfLamps = 10;
+    public float distanceBetweenLamps = 2.0f;
+
+    void Start()
+    {
+        for (int i = 0; i < numberOfLamps; i++)
+        {
+            Vector3 spawnPosition = new Vector3(i * distanceBetweenLamps, 0, 0);
+            Instantiate(prefab, spawnPosition, Quaternion.identity);
+        }
+    }
+
     void Update()
     {
     if (Input.GetKeyDown(KeyCode.B))
