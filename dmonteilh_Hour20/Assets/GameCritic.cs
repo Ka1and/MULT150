5c2c92785a6acf24b32c7ecbb28f88bce651ffff
Ka1and <darrianmonteilh@gmail.com>
2024-12-15 19:18:12 -0800

    Hour 20

---
+++ dmonteilh_Hour20/Assets/GameCritic.cs
@@ -0,0 +1,9 @@
+/* 1. I would say that the game is way too easy. I think that hitting the obstacles need to be a bit more punishing. Another reason I think the game is too easy is that it is just too slow.
+   2. There should be a health system so that when you hit a obstacle you lose a point of health. The speed of the game could also gradually increse with how long you stay alive.
+   3. I think a cool animation playing when you phase could give it a "wow" factor. The game could be zoomed out a little to show a cool skybox that has the same setting as the play area.
+   4. It is fun to see how long I can keep a run going. It is nice to dodge and weave through the obstacles and collect the power ups.
+   5. The speed of the game is defently very tedious. The movement is also another thing that is tedious with it feeling kind of floaty.
+
+   I changed the obstacles to try and make them ghosts.
+   For the powerup I changed the color of the particles and how they pop out of the sphere.
+*/
