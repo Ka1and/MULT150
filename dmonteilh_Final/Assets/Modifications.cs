b3d528460f2bdff2592516eea16510e0fc101ebb
Ka1and <darrianmonteilh@gmail.com>
2024-12-16 00:50:48 -0800

    Final 2

--- dmonteilh_Final/Assets/Modifications.cs
+++ dmonteilh_Final/Assets/Modifications.cs
@@ -1,18 +1,12 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Modifications : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-}
+/*
+   1. Changed directional light to purple and incresed intensity to 1.5
+   2. Added fog made the color to dark blue
+   3. Changed atmosphere thickness to .65 an made the sky tint dark purple
+   4. Made the obstacles Ghosts
+   5. Have the eyes under the head an the head under the obstacle
+   6. Added textures to the eyes
+   7. added particle system changed shape, size over lifetime, emission, and renderer
+   8. Changed the intensity to 1.3 and the range to 15
+   9. Converted wall to prefab and created a variant to replace the other wall
+   10. Modified the variant wall to purple
+*/
